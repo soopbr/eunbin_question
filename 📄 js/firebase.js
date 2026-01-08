7b51010105859b273175e98dc9301870e6bd1705
eunbin <thdgml2339@naver.com>
2026-01-08 13:00:21 +0900

    Simplify Firebase initialization
    Removed duplicate Firebase initialization check.

--- 📄 js/firebase.js
+++ 📄 js/firebase.js
@@ -1,4 +1,3 @@
-// Firebase 공통 설정
 const firebaseConfig = {
   apiKey: "AIzaSyDtDtadwFVoYMSahJyeNKqh-3NfwSagi-o",
   authDomain: "eunbin-4ba29.firebaseapp.com",
@@ -8,9 +7,5 @@ const firebaseConfig = {
   appId: "1:1042774764206:web:1068894ff8f3c583ef5b3f",
 };
 
-// Firebase 초기화 (중복 방지)
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
-
+firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
