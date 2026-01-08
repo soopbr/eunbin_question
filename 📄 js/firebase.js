344ebfe3951996e0f7294599f9700b8f9f0cc653
eunbin <thdgml2339@naver.com>
2026-01-08 12:14:20 +0900

    Create firebase.js

---
+++ 📄 js/firebase.js
@@ -0,0 +1,16 @@
+<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore-compat.js"></script>
+
+<script>
+  const firebaseConfig = {
+    apiKey: "AIzaSyDtDtadwFVoYMSahJyeNKqh-3NfwSagi-o",
+    authDomain: "eunbin-4ba29.firebaseapp.com",
+    projectId: "eunbin-4ba29",
+    storageBucket: "eunbin-4ba29.firebasestorage.app",
+    messagingSenderId: "1042774764206",
+    appId: "1:1042774764206:web:1068894ff8f3c583ef5b3f",
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
+</script>
