a3c752275f568651bccde5320638a13c216c412a
eunbin <thdgml2339@naver.com>
2026-01-08 12:27:38 +0900

    Update firebase.js

--- 📄 js/firebase.js
+++ 📄 js/firebase.js
@@ -1,16 +1,16 @@
-<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore-compat.js"></script>
+/***** Firebase SDK 로드 *****/
+importScripts?.(); // ❌ 이거 쓰지 마세요 (설명용)
 
-<script>
-  const firebaseConfig = {
-    apiKey: "AIzaSyDtDtadwFVoYMSahJyeNKqh-3NfwSagi-o",
-    authDomain: "eunbin-4ba29.firebaseapp.com",
-    projectId: "eunbin-4ba29",
-    storageBucket: "eunbin-4ba29.firebasestorage.app",
-    messagingSenderId: "1042774764206",
-    appId: "1:1042774764206:web:1068894ff8f3c583ef5b3f",
-  };
+/***** Firebase 설정 *****/
+const firebaseConfig = {
+  apiKey: "AIzaSyDtDtadwFVoYMSahJyeNKqh-3NfwSagi-o",
+  authDomain: "eunbin-4ba29.firebaseapp.com",
+  projectId: "eunbin-4ba29",
+  storageBucket: "eunbin-4ba29.firebasestorage.app",
+  messagingSenderId: "1042774764206",
+  appId: "1:1042774764206:web:1068894ff8f3c583ef5b3f",
+};
 
-  firebase.initializeApp(firebaseConfig);
-  const db = firebase.firestore();
-</script>
+/***** Firebase 초기화 *****/
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
